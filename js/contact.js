69e9ec5c3aeb2a59d359e54e779285542022883e
David De Castro <segphault@gmail.com>
2023-04-24 18:46:46 +0200

    Ajout d'un modal pour la partie contact dans index.html

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,31 @@
+// SECTION MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnContact");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// SECTION VALIDATION FORMULAIRE
 patternEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function css(element, style) {
@@ -25,11 +53,11 @@ function checkForm() {
     } else {
         css(document.querySelector('.prenomError'),{
             'visibility': 'hidden'
-        });   
+        });
         css(prenom,{
             'background-color': '#CCCCCC',
             'color': 'black'
-        });           
+        });
     }
 
     if (nom.value=='' || nom.value.length>150) {
@@ -44,11 +72,11 @@ function checkForm() {
     } else {
         css(document.querySelector('.nomError'),{
             'visibility': 'hidden'
-        }); 
+        });
         css(nom,{
             'background-color': '#CCCCCC',
             'color': 'black'
-        });           
+        });
     }
 
     if (email.value=='' || nom.value.length>30 || email.value.match(patternEmail)== null) {

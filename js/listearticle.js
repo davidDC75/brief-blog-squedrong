78ae05a4e068a679e2c4c46cd117a4a5649835ce
David De Castro <segphault@gmail.com>
2023-04-26 10:46:52 +0200

    Ajout de data.json et chargement depuis listearticle.js

--- js/listearticle.js
+++ js/listearticle.js
@@ -1,85 +1,12 @@
+let requestURL = 'js/data.json'; // Nom de ton fichier
+let request= new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType='json';
+request.send();
+
+
 container = document.querySelector(".article-preview");
 listeArticle = "";
-
-data = [
-    {
-        "image": "article1.jpg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    },
-
-    {
-        "image": "article2.jpeg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    },
-    {
-        "image": "article1.jpg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    },
-
-    {
-        "image": "article2.jpeg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    },
-    {
-        "image": "article1.jpg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    },
-
-    {
-        "image": "article2.jpeg",
-        "lien":"article1.html", 
-    
-        "date": "13 JAN 2022",
-
-        "title": "Lorem ipsum dolor sit amet",
-
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies mi facilisis libero sollicitudin vulputate. Mauris facilisis vestibulum nisi, at viverra arcu interdum id.",
-
-        "more": "EN SAVOIR PLUS"
-    }
-]
-
-
 
 function article(image, lien, date, title, description, more) {
     return `<div class="article-preview-index-6">
@@ -91,8 +18,10 @@ function article(image, lien, date, title, description, more) {
                 </div>`
 }
 
-for(i=0; i<data.length; i++) {
-    listeArticle = listeArticle + article(data[i].image, data[i].lien, data[i].date, data[i].title, data[i].description, data[i].more);
+request.onload = function() {
+    let data = request.response;
+    for(i=0; i<data.length; i++) {
+        listeArticle = listeArticle + article(data[i].image, data[i].lien, data[i].date, data[i].title, data[i].description, data[i].more);
+    }
+    container.innerHTML = listeArticle;
 }
-
-container.innerHTML = listeArticle;
